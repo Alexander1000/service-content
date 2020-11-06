e239c748ccd213efbf60203f43ac124a3c206831
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-11-06 11:21:43 +0300

    content save

--- include/srv-content/storage.h
+++ include/srv-content/storage.h
@@ -1,10 +1,16 @@
 #ifndef SRV_CONTENT_STORAGE_H
 #define SRV_CONTENT_STORAGE_H
 
+#include <srv-content/connection.h>
+
 namespace Content {
     class Storage {
     public:
-        Storage();
+        Storage(DBConn* db_conn);
+        void save_content(int* content_id, char* title, char* text, int user_id);
+
+    private:
+        DBConn* db_conn;
     };
 }
 
--- src/storage.cpp
+++ src/storage.cpp
@@ -1,1 +1,15 @@
 #include <srv-content/storage.h>
+#include <srv-content/connection.h>
+
+namespace Content
+{
+    Storage::Storage(DBConn* db_conn)
+    {
+        this->db_conn = db_conn;
+    }
+
+    void Storage::save_content(int *content_id, char *title, char *text, int user_id)
+    {
+        // do save
+    }
+}
