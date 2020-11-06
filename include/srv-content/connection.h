6c0561d491818afa644ddf34d10263a236f8bb05
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-11-06 11:13:02 +0300

    make connection

--- include/srv-content/connection.h
+++ include/srv-content/connection.h
@@ -8,7 +8,13 @@ namespace Content
     class DBConn {
     public:
         DBConn(const char* host, int port, const char* db_user, const char* db_password, const char* db_name);
-        pqxx::connection* conn;
+        pqxx::connection* get_conn();
+    private:
+        char* host;
+        int port;
+        char* db_user;
+        char* db_password;
+        char* db_name;
     };
 }
 
--- src/connection.cpp
+++ src/connection.cpp
@@ -1,1 +1,22 @@
 #include <srv-content/connection.h>
+#include <pqxx/pqxx>
+
+namespace Content
+{
+    DBConn::DBConn(const char *host, int port, const char *db_user, const char *db_password, const char *db_name)
+    {
+        this->host = (char*) host;
+        this->port = port;
+        this->db_user = (char*) db_user;
+        this->db_password = (char*) db_password;
+        this->db_name = (char*) db_name;
+    }
+
+    pqxx::connection* DBConn::get_conn()
+    {
+        char* conn_str = new char[128];
+        memset(conn_str, 0, sizeof(char) * 128);
+        sprintf(conn_str, "postgres://%s@%s:%d/%s", this->db_user, this->host, this->port, this->db_name);
+        return new pqxx::connection(conn_str);
+    }
+}
