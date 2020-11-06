c0d4781d4ef164b362fb0340aedd90d417184d2e
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-11-06 13:48:46 +0300

    add config

---
+++ include/srv-content/config.h
@@ -0,0 +1,13 @@
+#ifndef SRV_CONTENT_CONFIG_H
+#define SRV_CONTENT_CONFIG_H
+
+namespace Content
+{
+    class Config
+    {
+    public:
+        Config(int argc, char **argv);
+    };
+}
+
+#endif
---
+++ src/config.cpp
@@ -0,0 +1,9 @@
+#include <srv-content/config.h>
+
+namespace Content
+{
+    Config::Config(int argc, char **argv)
+    {
+        // do nothing
+    }
+}
