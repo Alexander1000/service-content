dd9d6f39aaa1aee10db0dfb27b2d15427644cade
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-11-06 13:54:27 +0300

    add config

--- include/srv-content/config.h
+++ include/srv-content/config.h
@@ -1,12 +1,18 @@
 #ifndef SRV_CONTENT_CONFIG_H
 #define SRV_CONTENT_CONFIG_H
 
+#include <string>
+
 namespace Content
 {
     class Config
     {
     public:
         Config(int argc, char **argv);
+    private:
+        bool is_help;
+
+        void parse_config_file(std::string file_name);
     };
 }
 
--- src/config.cpp
+++ src/config.cpp
@@ -1,9 +1,35 @@
 #include <srv-content/config.h>
+#include <string>
 
 namespace Content
 {
     Config::Config(int argc, char **argv)
     {
-        // do nothing
+        this->is_help = false;
+
+        for (int i = 1; i < argc; i++) {
+            std::string paramName(argv[i]);
+
+            if (paramName == "-h" || paramName == "--help") {
+                this->is_help = true;
+                continue;
+            }
+
+            if (paramName == "-c" || paramName == "--config") {
+                if (i + 1 == argc) {
+                    // todo: error missed argument value
+                    continue;
+                }
+                std::string paramValue(argv[i+1]);
+                this->parse_config_file(paramValue);
+                i++;
+                continue;
+            }
+        }
+    }
+
+    void Config::parse_config_file(std::string file_name)
+    {
+        // do parse
     }
 }
