fd6628625c10d33d1bd3f6c6a45fee227c39ff5c
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-11-06 12:11:01 +0300

    fetch version

--- src/storage.cpp
+++ src/storage.cpp
@@ -15,16 +15,23 @@ namespace Content
         pqxx::connection* conn = this->db_conn->get_conn();
         pqxx::work txn{*conn};
 
+        int version = 0;
+
         if (content_id == nullptr) {
             // create new content
             content_id = new int;
 
             pqxx::row row = txn.exec1("insert into content(version) values(0) returning id");
 
-            for (auto column = row.begin(); column != row.end(); column++) {
-                *content_id = column.as<int>();
-                break;
-            }
+            auto column = row.begin();
+            *content_id = column.as<int>();
+        } else {
+            char* query = new char[128];
+            memset(query, 0, sizeof(char) * 128);
+            sprintf(query, "select version from content where id = %d", *content_id);
+            pqxx::row r = txn.exec1(query);
+            auto column = r.begin();
+            version = column.as<int>();
         }
 
         txn.commit();
