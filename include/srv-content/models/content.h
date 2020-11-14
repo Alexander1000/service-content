cee2c0619b83a79cce111a6a9cb786f9dc7fa0a4
Aleksandr Dankovtsev <a.dankovtsev@mail.ru>
2020-11-14 11:57:08 +0300

    add model content

---
+++ include/srv-content/models/content.h
@@ -0,0 +1,16 @@
+#ifndef CONTENT_MODEL_H
+#define CONTENT_MODEL_H
+
+namespace Content::Model
+{
+    class Content
+    {
+    public:
+        int* id;
+        char* title;
+        char* text;
+        int user_id;
+    };
+}
+
+#endif
