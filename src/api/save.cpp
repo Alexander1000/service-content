9c39093349a4c747b73c2973b62b185d07c238cc
Aleksandr Dankovtsev <a.dankovtsev@mail.ru>
2020-11-14 11:51:36 +0300

    add 405 error code

--- src/api/save.cpp
+++ src/api/save.cpp
@@ -11,6 +11,14 @@ namespace Content::API
 
     void SaveHandler::on_request(Socketer::Request *request, Socketer::Response* resp)
     {
+        if (std::string(request->method) != "POST") {
+            resp->writeHead("HTTP/1.1 405 Method not allowed");
+            resp->addHeader("Content-Type", "application/json");
+            std::string errResp = "{\"error\":{\"code\":405,\"message\":\"Method not allowed\"}}";
+            resp->write((void*) errResp.c_str(), errResp.length());
+            resp->reply();
+            return;
+        }
         std::cout << "/v1/save called" << std::endl;
 
         std::cout << "Raw body: [" << request->raw_body << "]" << std::endl;
