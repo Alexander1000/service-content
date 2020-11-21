bb23e040c7566c21cda67e073c2778f946be6d5a
Aleksandr Dankovtsev <a.dankovtsev@mail.ru>
2020-11-21 11:44:55 +0300

    reply response on page save

--- src/api/page/save.cpp
+++ src/api/page/save.cpp
@@ -8,6 +8,26 @@ namespace Content::API::Page
     }
 
     void SaveHandler::on_request(Socketer::Request *request, Socketer::Response *resp) {
+        if (std::string(request->method) != "POST") {
+            resp->writeHead("HTTP/1.1 405 Method not allowed");
+            resp->addHeader("Content-Type", "application/json");
+            this->error_response(resp, 405, (char*) "Method not allowed");
+            resp->reply();
+            return;
+        }
 
+        if (std::string(request->headers["Content-Type"]) != "application/json") {
+            resp->writeHead("HTTP/1.1 405 Method not allowed");
+            resp->addHeader("Content-Type", "application/json");
+            this->error_response(resp, 405, (char*) "Method not allowed");
+            resp->reply();
+            return;
+        }
+
+        RequestSave* reqSave = this->parse_request(request);
+
+        resp->writeHead("HTTP/1.1 200 OK");
+        resp->addHeader("Content-Type", "application/json; charset=utf-8");
+        resp->reply();
     }
 }
