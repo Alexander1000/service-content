dfebe6b2bec28defc81aeb5c16edd6f64b37fa50
Aleksandr Dankovtsev <a.dankovtsev@mail.ru>
2020-11-22 13:51:21 +0300

    call view handler

--- src/api/view/handler.cpp
+++ src/api/view/handler.cpp
@@ -5,4 +5,8 @@ namespace Content::API::View
     Handler::Handler(Content::Storage *storage) {
         this->storage = storage;
     }
+
+    void Handler::on_request(Socketer::Request *request, Socketer::Response *response) {
+
+    }
 }
