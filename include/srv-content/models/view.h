1d90f83ac0b321b90db06e552f82b904f07e3a20
Aleksandr Dankovtsev <a.dankovtsev@mail.ru>
2020-11-22 15:39:00 +0300

    add model view

---
+++ include/srv-content/models/view.h
@@ -0,0 +1,10 @@
+#ifndef SRV_CONTENT_MODEL_VIEW_H
+#define SRV_CONTENT_MODEL_VIEW_H
+
+namespace Content::Model
+{
+    class View
+    {};
+}
+
+#endif
---
+++ src/models/view.cpp
@@ -0,0 +1,6 @@
+#include <srv-content.h>
+
+namespace Content::Model
+{
+
+}
