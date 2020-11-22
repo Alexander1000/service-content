944c3c86129e026b0cebe5e58d482f2e669923bc
Aleksandr Dankovtsev <a.dankovtsev@mail.ru>
2020-11-22 18:24:15 +0300

    implement parse request for view

--- src/api/view/parse_request.cpp
+++ src/api/view/parse_request.cpp
@@ -1,10 +1,38 @@
 #include <socketer.h>
 #include <srv-content.h>
+#include <io-buffer.h>
+#include <json-stream-analyzer.h>
 
 namespace Content::API::View
 {
     Request* Handler::parse_request(Socketer::Request *request)
     {
-        return nullptr;
+        IOBuffer::IOMemoryBuffer memoryBuffer;
+        memoryBuffer.write(request->raw_body, strlen(request->raw_body));
+        memoryBuffer.setPosition(0);
+
+        IOBuffer::CharStream charStream(&memoryBuffer);
+        JsonStreamAnalyzer::Stream stream(&charStream);
+        JsonStreamAnalyzer::Decoder decoder(&stream);
+
+        JsonStreamAnalyzer::Element* element = decoder.decode();
+
+        Request* reqView = nullptr;
+        reqView = new Request;
+
+        if (element->getType() == ELEMENT_TYPE_OBJECT) {
+            auto obj = (JsonObject*) element->getData();
+            if (obj->find("slug") != obj->end()) {
+                auto elSlug = obj->at("slug");
+                if (elSlug->getType() == ELEMENT_TYPE_TEXT) {
+                    auto slug = (std::string*) elSlug->getData();
+                    reqView->slug = new char[slug->length() + 1];
+                    memset(reqView->slug, 0, (slug->length() + 1) * sizeof(char));
+                    memcpy(reqView->slug, slug->c_str(), slug->length() * sizeof(char));
+                }
+            }
+        }
+
+        return reqView;
     }
 }
